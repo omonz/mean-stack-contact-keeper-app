1075df184e040be229e1ee7c8401ccbd67d0bda0
Emmanuel <emmizy2015@gmail.com>
2020-04-14 19:52:41 +0100

    auth system created and ready to add the backend logic

--- client/src/context/alert/AlertContext.js
+++ client/src/context/alert/AlertContext.js
@@ -3,4 +3,4 @@ import { createContext } from 'react';
 
 const alertContext  = createContext();
 
-export default alertContext;
+export default alertContext; 
--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -13,7 +13,7 @@ const AlertState = props => {
 
     //Set Alert
     const setAlert = (msg, type, timeout = 5000) => {
-        const id = uuid.v4();
+        const id = uuid;
         dispatch({type: SET_ALERT, payload: {msg, type, id}});
 
         setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id}), timeout);
