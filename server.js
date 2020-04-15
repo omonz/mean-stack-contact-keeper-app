ce86b1377c68d7fa061f34ccc081b8d16416a581
Emmanuel <emmizy2015@gmail.com>
2020-04-16 00:55:40 +0100

    setting the environment varriable and deployment

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 
 //connect database
 connectDB();
-
-app.get('/', (req, res) => 
-    res.json({ greetings: 'this is noce'})
-); 
 
 app.use(express.json({ extended: false }));
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contact'));
 app.use('/api/users', require('./routes/users'));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder 
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
